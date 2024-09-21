acb5e1e5047096924260d9a9d5af63ff5dc9e42d
Puspalal <puspalalnewar79@gmail.com>
2024-09-21 20:31:50 +0530

    Created a Power Calculator

---
+++ app.js
@@ -0,0 +1,20 @@
+let num = document.querySelector("#num");
+let powx = document.querySelector("#pow");
+const calculate = document.querySelector("#calculate");
+const reset = document.querySelector("#reset");
+const ans = document.querySelector(".ans-box");
+
+const calRes = () => {
+    return Math.pow(num.value,powx.value);
+} 
+
+calculate.addEventListener("click", ()=>{
+    console.log(calRes());
+    ans.innerHTML = `Ans : ${num.value}<sup>${powx.value}</sup> = ${calRes()}`
+})
+
+reset.addEventListener("click", ()=>{
+    ans.innerHTML = "Ans : ";
+    num.value = "";
+    powx.value = "";
+})
